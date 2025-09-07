850ac76b698a4af59a4ac3e27008390921aabf43
Aleksey Tyurin <a.tyurin@teachbase.ru>
2025-09-07 20:28:36 +0300

    lint all files and add slim-lint configuration

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -4,13 +4,13 @@ class Web::ApplicationController < ApplicationController
   include AuthConcern
 
   private
-  
+
   def require_authentication
     user_not_authorized unless signed_in?
   end
 
   def user_not_authorized
-    flash[:alert] = t('alerts.not_authorized')
+    flash[:alert] = t('notices.user.not_authorized')
     redirect_back(fallback_location: root_path)
   end
 end

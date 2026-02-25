82b2505e84fc9c2d1e1ec89bee2a75e8d2729e64
Aleksey Tyurin <a.tyurin@teachbase.ru>
2026-02-25 19:15:22 +0300

    fix tests

--- test/controllers/web/bulletins_controller_test.rb
+++ test/controllers/web/bulletins_controller_test.rb
@@ -22,6 +22,24 @@ module Web
       assert_select 'a[href=?]', bulletin_path(@published_bulletin)
     end
 
+    test 'admin link is hidden for not admin' do
+      user = users(:one)
+      sign_in(user)
+
+      get root_path
+      assert_response :success
+      assert_no_match 'admin', response.body.downcase
+    end
+
+    test 'admin link is shown to admin' do
+      user = users(:two)
+      sign_in(user)
+
+      get root_path
+      assert_response :success
+      assert_match 'admin', response.body.downcase
+    end
+
     test 'should not show draft bulletins on index' do
       get bulletins_url
 
